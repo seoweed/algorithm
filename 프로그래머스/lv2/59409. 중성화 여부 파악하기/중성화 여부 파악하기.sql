f2b54cfb8af44c9475584a65526b75ee0c86d38e
seoweed <129758184+seoweed@users.noreply.github.com>
2023-04-26 08:39:55 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,8 @@
+select ANIMAL_ID, NAME,
+    case 
+        when (SEX_UPON_INTAKE like '%Neutered%' 
+            or SEX_UPON_INTAKE like '%Spayed%') then 'O'
+        else 'X'
+    end '중성화'
+    from ANIMAL_INS 
+    order by ANIMAL_ID;
