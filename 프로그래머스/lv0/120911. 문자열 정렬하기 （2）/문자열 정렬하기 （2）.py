dd795d9ec4b22cc4dd04dba7b970de2ef0fc9ff1
seoweed <129758184+seoweed@users.noreply.github.com>
2023-05-09 18:48:42 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,6 +1,7 @@
 def solution(my_string):
     answer = ''
     a = sorted(my_string.lower())
-    for i in a:
-        answer += i
+    # for i in a:
+    #     answer += i
+    answer = "".join(a)
     return answer
