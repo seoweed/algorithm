cfc391f15df039e82b0be7094399f0978dc4c111
seoweed <129758184+seoweed@users.noreply.github.com>
2023-05-11 10:43:29 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,18 @@
+def solution(num):
+    answer = 0
+    count = 0
+    if num == 1:
+        return 0;
+    else:
+        while num != 1:
+            if num % 2 == 0:
+                num = num / 2
+                count += 1
+                if count >= 500:
+                    return -1
+            else:
+                num = num * 3 + 1
+                count += 1
+                if count >= 500:
+                    return -1
+        return count
