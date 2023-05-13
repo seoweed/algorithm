c548d5b03ea79ee4398a4347743c6e5bddd74101
seoweed <129758184+seoweed@users.noreply.github.com>
2023-05-14 07:45:21 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = ''
+    i = 0
+    while (i < n):
+        if i % 2 == 0:
+            answer += '수'
+        else:
+            answer += '박'
+        i += 1
+        
+    return answer
