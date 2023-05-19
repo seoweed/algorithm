49b0ee1d2c3a4b83c5b57f2250d4ab586cf78416
seoweed <129758184+seoweed@users.noreply.github.com>
2023-05-20 07:29:33 +0900

    [unrated] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,13 @@
+def solution(arr1, arr2):
+    if len(arr1) != len(arr2):
+        if len(arr1) > len(arr2):
+            return 1
+        else :
+            return -1
+    else:
+        if sum(arr1) == sum(arr2):
+            return 0
+        elif sum(arr1) > sum(arr2):
+            return 1
+        else:
+            return -1
