8f5b06038ba607b37df93651f62bb5585aa95c7d
seoweed <129758184+seoweed@users.noreply.github.com>
2023-06-18 07:26:50 +0900

    [unrated] Title: 수 조작하기 2, Time: 21.65 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,12 @@
+def solution(numLog):
+    answer = ''
+    for i in range(1, len(numLog)):
+        if numLog[i] == numLog[i - 1] + 1:
+            answer += 'w'
+        elif numLog[i] == numLog[i - 1] - 1:
+            answer += 's'
+        elif numLog[i] == numLog[i - 1] + 10:
+            answer += 'd'
+        else:
+            answer += 'a'
+    return answer
