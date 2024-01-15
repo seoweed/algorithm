4539121ab3cfc9e11f77568eef3fc119065ac383
seoweed <129758184+seoweed@users.noreply.github.com>
2024-01-15 19:43:21 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17968 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        double inputA = scanner.nextDouble();
+        double inputB = scanner.nextDouble();
+        
+        System.out.println(inputA / inputB);
+    }
+}
