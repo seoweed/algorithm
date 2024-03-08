353e9f7465d2ab3570872c44d56f5679bbb3e63a
seoweed <129758184+seoweed@users.noreply.github.com>
2024-03-08 21:20:07 +0900

    [Bronze IV] Title: 영수증, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,11 @@
+x = int(input())
+n = int(input())
+result = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    result = result + a * b
+if result == x:
+    print('Yes')
+else:
+    print('No')
