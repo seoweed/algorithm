ff336c6a0d23a024acf57bbba8a82350b8a8cf2c
seoweed <129758184+seoweed@users.noreply.github.com>
2024-05-31 09:53:22 +0900

    [Silver II] Title: 스택 수열, Time: 1072 ms, Memory: 113908 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+        Stack<Integer> stack = new Stack<>();
+        int num = 1;
+        boolean result = true;
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            int su = A[i];
+            if (su >= num) {
+                while (su >= num) {
+                    stack.push(num++);
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                int temp = stack.pop();
+                if (temp > su) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                } else {
+                    sb.append("-\n");
+                }
+            }
+        }
+        if (result) {
+            System.out.println(sb.toString());
+
+        }
+    }
+}
