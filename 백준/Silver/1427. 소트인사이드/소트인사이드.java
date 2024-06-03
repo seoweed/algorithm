d4017951bcf8010787a5b299b226a60da37df868
seoweed <129758184+seoweed@users.noreply.github.com>
2024-06-03 14:32:51 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -2,14 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        Integer[] A = new Integer[str.length()];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] A = new int[str.length()];
         for (int i = 0; i < str.length(); i++) {
             A[i] = Integer.parseInt(str.substring(i, i + 1));
         }
-        Arrays.sort(A, Collections.reverseOrder());
+        // 정렬 알고리즘 (선택정렬)
+        for (int i = 0; i < str.length(); i++) {
+            int max = i;
+            for (int j = i + 1; j < str.length(); j++) {
+                if (A[j] > A[max]) {
+                    max = j;
+                }
+            }
+            if (A[i] < A[max]) {
+                int temp = A[i];
+                A[i] = A[max];
+                A[max] = temp;
+            }
+        }
         for (int i : A) {
             System.out.print(i);
         }
