c35a1bf0c60b390ca91ef4e472a0d65c8a27ccfc
seoweed <129758184+seoweed@users.noreply.github.com>
2024-06-03 19:49:34 +0900

    [Silver IV] Title: 제로, Time: 252 ms, Memory: 23852 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+        int result = 0;
+        for (int i : stack) {
+            result += i;
+        }
+        System.out.println(result);
+    }
+}
