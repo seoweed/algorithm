98f8e5ddb4abb8bee3114cec722e8637fe344783
seoweed <129758184+seoweed@users.noreply.github.com>
2024-06-04 18:23:35 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 704 ms, Memory: 141252 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static boolean[] visited;
+    static ArrayList<Integer>[] A;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n + 1];
+        A = new ArrayList[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            A[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
+        }
+        int count = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (!visited[i]) {
+                count++;
+                DFS(i);
+            }
+        }
+        System.out.println(count);
+    }
+    private static void DFS(int v) {
+        if (visited[v]) return;
+        visited[v] = true;
+        for (int i : A[v]) {
+            if (!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
+}
