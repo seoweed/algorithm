6636fb1ea165cde7772943c5fe8cc441587be4bb
seoweed <129758184+seoweed@users.noreply.github.com>
2024-06-10 13:07:09 +0900

    [Gold V] Title: 집합의 표현, Time: 2556 ms, Memory: 253600 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        parent = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < M; i++) {
+            int question = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (question == 0) {
+                // union
+                union(a, b);
+            } else {
+                boolean result = checkSame(a, b);
+                if (result) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+    private static int find(int a) {
+        if (a == parent[a]) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    private static boolean checkSame(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return true;
+        return false;
+    }
+}
