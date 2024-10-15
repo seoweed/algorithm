33692415a01e87098f14abbfdeaa8b3ed287af3d
seoweed <129758184+seoweed@users.noreply.github.com>
2024-10-15 22:03:27 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer>[] A;
+    static boolean[] visited;
+    static int result = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        A = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i < n + 1; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
+        }
+
+        DFS(1);
+        System.out.println(result);
+    }
+    public static void DFS(int v) {
+        if (visited[v]) {
+            return;
+        }
+        visited[v] = true;
+        for (int i : A[v]) {
+            if (!visited[i]) {
+                result++;
+                DFS(i);
+            }
+        }
+    }
+}
