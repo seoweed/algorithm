46c9066a85dbf8de07ad7f31917a3e6ef0ab4b25
Juhyeon Seo <129758184+seoweed@users.noreply.github.com>
2024-11-14 08:39:05 +0900

    [Silver I] Title: 회의실 배정, Time: 464 ms, Memory: 42532 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int[][] A;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        A = new int[n][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            A[i][0] = Integer.parseInt(st.nextToken());
+            A[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A, (a, b) -> {
+            if (a[1] == b[1]) return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        int count = 0;
+        int endTime = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (A[i][0] >= endTime) {
+                count++;
+                endTime = A[i][1];
+            }
+        }
+        System.out.println(count);
+
+    }
+
+}
