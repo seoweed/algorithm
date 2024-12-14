b6e8cd00b01f6277597f6847a78376ec68af7177
Juhyeon Seo <129758184+seoweed@users.noreply.github.com>
2024-12-14 17:28:04 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 96 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        
+        switch(input) {
+            case "NLCS":
+                System.out.println("North London Collegiate School");
+                break;
+            case "BHA":
+                System.out.println("Branksome Hall Asia");
+                break;
+            case "KIS":
+                System.out.println("Korea International School");
+                break;
+            case "SJA":
+                System.out.println("St. Johnsbury Academy");
+                break;
+        }
+        
+    }
+}
