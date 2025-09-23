6c99cd6984707492f459557c0c43d5c698450444
Juhyeon Seo <129758184+seoweed@users.noreply.github.com>
2025-09-24 08:05:37 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 170.30 ms, Memory: 63.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char[] arr = a.toCharArray();
+        
+        for (char c : arr) {
+            if (Character.isUpperCase(c)) {
+                System.out.print(Character.toLowerCase(c));
+            } else {
+                System.out.print(Character.toUpperCase(c));
+            }
+        }
+    }
+}
